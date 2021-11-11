e5e94f5ff4b3ece87120ee61849689abb2fec70c
timdika <timofej.kazakov.privat@gmail.com>
2021-11-11 13:49:09 +0100

    Vorbereitung Keras

--- Versuchtslabor MA/ganzVersucheKERAS.py
+++ Versuchtslabor MA/ganzVersucheKERAS.py
@@ -2,4 +2,11 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 import os
+import matplotlib.pyplot as plt
+
+bild_groesse = (1024, 1024)
+batch_groesse = 32
+
+trainings_ds = tf.keras.preprocessing.image_dataset_from_directory()
+val_ds = tf.keras.preprocessing.image_dataset_from_directory()
 
