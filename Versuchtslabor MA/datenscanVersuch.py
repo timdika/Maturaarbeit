1523c1b50e1e6edaaf3be1382b47e40d8a9450a8
timdika <timofej.kazakov.privat@gmail.com>
2021-12-14 12:18:06 +0100

    Laborversuche zu Grayscaling

--- Versuchtslabor MA/datenscanVersuch.py
+++ Versuchtslabor MA/datenscanVersuch.py
@@ -4,10 +4,12 @@ import os, sys
 import numpy as np
 from PIL import Image
 
-im = Image.open("testSchlechtRoent.jpg")
-im_array = np.array(im)
+im = Image.open("C:/Users/timof/Desktop/MA/Versuchtslabor MA/testSchlechtRoent.jpg").convert('L')
+im.save('pil-grayscale.png')
+#im_array = np.array(im)
 
-print(im_array.shape)
-print("")
-print(im_array) #Was macht das?
-
+#print(im_array.shape)
+#print("")
+#print(im_array) #Was macht das?
+#print(np.asarray(im)) #Ganze Liste von einzelnen Pixel - Jeder Pixel als drei Werte
+#print(im.mode)
