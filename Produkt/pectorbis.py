7a9402b7cb26ef1e058df4cb14790c6a60da343b
timdika <timofej.kazakov.privat@gmail.com>
2021-12-27 18:16:34 +0100

    Mit 20 Bildern arbeiten - Pectorbis bis Datashuffling

--- Produkt/pectorbis.py
+++ Produkt/pectorbis.py
@@ -3,5 +3,39 @@ import numpy as np
 import matplotlib.pyplot as plt
 import cv2
 
-labels = os.listdir("C:/Users/timof/Desktop/Dokumente und Datensatz für Maturaarbet/ZweiFallDatensatz/train")
-print(labels)
+
+
+
+def Datensatz_laden(datensatz, path):
+
+    labels = os.listdir(os.path.join(path, datensatz))
+
+    X = []
+    y = []
+
+    for label in labels:
+        for file in os.listdir(os.path.join(path, datensatz, label)):
+            image = cv2.imread(os.path.join(path, datensatz, label, file), cv2.IMREAD_UNCHANGED)
+
+            X.append(image)
+            y.append(label)
+
+    return np.array(X), np.array(y).astype('uint8')
+
+
+def create_data_med(path):
+
+    X, y = Datensatz_laden('train', path)
+    X_test, y_test = Datensatz_laden('test', path)
+
+    return X, y, X_test, y_test
+
+
+X, y, X_test, y_test = create_data_med('C:/Users/timof/Desktop/Dokumente_und_Datensatz_MA/ZweiFallDatensatz')
+
+X = (X.astype(np.float32) - 127.5) / 127.5
+X_test = (X_test.astype(np.float32) - 127.5) / 127.5
+
+X = X.reshape(X.shape[0], -1)
+X_test = X_test.reshape(X_test.shape[0], -1)
+
